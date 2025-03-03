bff45dc4f586dd767728b6ed176eed739a1bf549
ardaoru <160649630+ardaoru@users.noreply.github.com>
2025-03-03 13:51:50 +0300

    .

--- Week 3-2/test_factorial.py
+++ Week 3-2/test_factorial.py
@@ -1,7 +1,7 @@
 import factorial
 
 def test_4():
-    assert introduction.factorial(4) == 20
+    assert factorial.factorial(4) == 24
     
 def test_N1():
-    assert introduction.factorial(-1) == None
+    assert factorial.factorial(-1) == None
