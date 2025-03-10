ad182e04f81f95455a165f4f95e06c567a9d1914
ardaoru <ardaoru2001@gmail.com>
2025-03-10 14:28:45 +0300

    updates

--- Week 4-2/classintro.py
+++ Week 4-2/classintro.py
@@ -5,18 +5,22 @@ def f():
 
 f()
 class Apple:  # class, concept
-    type = "fruit"      
-    location = "tree" # should be different for each instance
-    def drop(self, l):
-        self.location = l
+    type = "fruit"    
+    def __init__(self, loc = "tree", clr = "red"): # biz bir bilgi vermezsek default olarak bunları al
+        self.location = loc
+        self.color = clr
+
+    def drop(self):
         if self.location == "tree":
             print("I dropped")
         else:
-        print("I cannot be dropped")   
+            print("I cannot be dropped")   
 
-x = Apple() # object, tangible
-y = Apple()
+x = Apple("tree", "red") # object, tangible
+y = Apple(loc = "ground")
+z = Apple(clr = "yellow")
 
-x.drop("tree") # Actually calls Apple.drop(x)
+x.drop() # Actually calls Apple.drop(x)
+y.drop()
 
 print (x.type)
